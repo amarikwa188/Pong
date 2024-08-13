b3e126cad94d0a23c3ce62d00daa625a98e0149f
Amarikwa <amarikwa188@gmail.com>
2024-08-13 08:46:03 +0100

    commented settings class

--- settings.py
+++ settings.py
@@ -4,15 +4,19 @@ class GameSettings:
         """
         Initializes a settings object.
         """
+        # screen dimensions
         self.screen_height: int = 350
         self.screen_width: int = 550
 
+        # colors
         self.bg_color: tuple[int, int, int] = (0,0,0)
         self.fg_color: tuple[int, int, int] = (255,255,255)
 
+        # paddle dimensions
         self.paddle_height: int  = 70
         self.paddle_width: int = 10
 
+        # speed settings
         self.player_speed: float = 1.7
         self.cpu_speed: float = 1.3
-        self.ball_speed: float = 0.9
+        self.ball_speed: float = 1.0
