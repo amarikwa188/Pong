7b36bdbf918738a94f159c1202db5ee023b04b2b
Amarikwa <amarikwa188@gmail.com>
2024-08-16 12:06:58 +0100

    fixed incorrect audio file name

--- audio_handler.py
+++ audio_handler.py
@@ -4,5 +4,5 @@ from pygame.mixer import Sound
 class AudioHandler:
     def __init__(self) -> None:
         self.bounce_sound: Sound = Sound("audio_files/pong_sfx.wav")
-        self.point_sound: Sound = Sound("audio_files/point3.mp3")
+        self.point_sound: Sound = Sound("audio_files/point.mp3")
         self.pause_sound: Sound = Sound("audio_files/pause.mp3")
